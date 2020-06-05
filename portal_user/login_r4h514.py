6a250f354f38bb1777a1a5214fd0101c30e6c778
posmaryan <posmapanggabean14@gmail.com>
2020-06-05 20:46:29 +0700

    update script login_r4h514.py

--- portal_user/login_r4h514.py
+++ portal_user/login_r4h514.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 import time
 
-browser = webdriver.Chrome(executable_path=r'C:\Users\Administrator\Documents\fast8\Selenium\drivers\chromedriver.exe')
+browser = webdriver.Chrome(executable_path=r'C:\Users\Posma Panggabean\PycharmProjects\drivers\chromedriver.exe')
 
 browser.set_page_load_timeout(10)
-browser.get('https://user.hadirr.com/r4h514/')
+browser.get('https://user.hadirr.com/r4h514/170700004')
 browser.find_element_by_name('email').send_keys('dara.sativa@fast-8.com')
 browser.find_element_by_name('password').send_keys('daraayus')
 #klik button login
